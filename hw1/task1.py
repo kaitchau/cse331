456de948f5b2eb5b255ceebed32f95559213f387
Kaitlyn Chau <91744930+kaitchau@users.noreply.github.com>
2024-09-18 16:35:53 -0400

    transfer to pc

--- hw1/task1.py
+++ hw1/task1.py
@@ -1,5 +1,11 @@
 plaintext = input("Enter plaintext:")
 cipherkey = input("Enter cipher key:")
 
+# Uppercase A-Z => 65-90 in ASCII
+# lowercase a-z => 97-122 in ASCII
+
+ct=0 # Keep ct of num of pt chars processed, & reset once it goes past the len of ck
 for char in plaintext:
-    # here
+    print(ord(char))
+    ct+=1
+
