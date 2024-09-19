9b9d35d2418a14ccef41a29b67cb12bade5fe263
Kaitlyn Chau <kaitlyn.chau@stonybrook.edu>
2024-09-19 02:14:15 -0400

    task1 done

--- hw1/task1.py
+++ hw1/task1.py
@@ -1,12 +1,43 @@
 # Vigenère cipher encoder
 plaintext = input("Enter plaintext:")
 cipherkey = input("Enter cipher key:")
-
+ciphertext = ''
 # Uppercase A-Z => 65-90 in ASCII
 # lowercase a-z => 97-122 in ASCII
 
 ct=0 # Keep ct of num of pt chars processed, & reset once it goes past the len of ck
 for char in plaintext:
-    print(ord(char))
-    ct+=1
+    if(char.isalpha()):
+        # Apply cipher key to alphabet letters
+        ascii = ord(char)
+        key = ord(cipherkey[ct])-65
+        # print('key:'+ str(key))
 
+        if(char.isupper()):
+            #handle wrap around for upper
+            ascii-=65
+        elif(char.islower()):
+            ascii-=97
+
+        #apply cipherkey to char
+        for i in range(key):
+            #here
+            ascii+=1
+            if ascii >= 26:
+                # wrap around
+                ascii=0
+        
+        if(char.isupper()):
+            # restore ascii to alphabet
+            ascii+=65
+        elif(char.islower()):
+            ascii+=97
+
+        ciphertext = ciphertext + chr(ascii)
+    else:
+        ciphertext = ciphertext + char
+    ct+=1
+    if ct>=len(cipherkey):
+        ct=0
+
+print(ciphertext)
